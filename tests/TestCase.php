3be018deb38a7f65482ad4dc134a362000ef9f6f
[Hoja J] <[hojajepbar@gmail.com]>
2024-11-01 16:10:16 +0500

    fix some issue

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+// GEMINI_API_KEY='AIzaSyBUHSP30_HDusM7Olo7Kde3InyLfvaWR1c'
+// MAIL_HOST=smtp.gmail.com
+// MAIL_PORT=587
+// mail_username='hojajepbar@gmail.com'
+// MAIL_PASSWORD='ycknrzmbtigruihp'
+// MAIL_ENCRYPTION=null
 }
